9850fe3367aa5f05be2a0d8f1e7176fb3d99e6dc
Jeremy Custenborder <jcustenborder@gmail.com>
2016-08-22 22:37:41 -0500

    Inital commit for jenkinsfile.

--- bin/debug.sh
+++ bin/debug.sh
@@ -20,4 +20,4 @@ mvn clean package
 export CLASSPATH="$(find `pwd`/target/kafka-connect-salesforce-1.0-SNAPSHOT-package/share/java/ -type f -name '*.jar' | tr '\n' ':')"
 
 
-$CONFLUENT_HOME/bin/connect-standalone connect/connect-avro-docker.properties config/salesforce.properties
+$CONFLUENT_HOME/bin/connect-standalone connect/connect-avro-docker.properties config/salesforce.activity.properties
